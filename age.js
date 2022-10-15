80aa6a275e3e16e4daff113b3d2ae0952d7eebdc
aloyuche <37053321+aloyuche@users.noreply.github.com>
2022-10-15 14:59:27 +0100

    Update age.js

--- age.js
+++ age.js
@@ -2,14 +2,14 @@ function daysOfAge() {
   var birthday = prompt("What is your Birth Year ----- my good friend?");
 }
 
-// function ageInDays() {
+function ageInDays() {
 
-//   // let birthYear = alert("What is your birth year? ... friend");
-//   // var age = (new Date() - birthYear) * 365;
-//   // var h2 = document.createElement("h2");
-//   // var textAnswer = document.createTextNode("You are " + age + "  days old");
+  let birthYear = alert("What is your birth year? ... friend");
+  var age = (new Date() - birthYear) * 365;
+  var h2 = document.createElement("h2");
+  var textAnswer = document.createTextNode("You are " + age + "  days old");
 
-//   h2.setAttribute("id", "ageInDays");
-//   h2.appendChild(textAnswer);
-//   document.getElementById("flex-box-result").appendChild(h2);
-// }
+  h2.setAttribute("id", "ageInDays");
+  h2.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h2);
+}
